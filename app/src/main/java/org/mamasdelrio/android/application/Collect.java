7cacd4bac3d35cb616b0578899766afb954e5893
Sam Sudar <sudar.sam@gmail.com>
2016-08-20 21:31:32 -0700

    Add analytics and reformat Application code

--- app/src/main/java/org/mamasdelrio/android/application/Collect.java
+++ app/src/main/java/org/mamasdelrio/android/application/Collect.java
@@ -21,6 +21,10 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+import org.mamasdelrio.android.BuildConfig;
 import org.mamasdelrio.android.R;
 import org.mamasdelrio.android.database.ActivityLogger;
 import org.mamasdelrio.android.external.ExternalDataManager;
@@ -45,194 +49,207 @@ import java.io.File;
  */
 public class Collect extends Application {
 
-    static {
-		PRNGFixes.apply();
-	}
-	
-    // Storage paths
-    public static final String ODK_ROOT = Environment.getExternalStorageDirectory()
-            + File.separator + "odk";
-    public static final String FORMS_PATH = ODK_ROOT + File.separator + "forms";
-    public static final String INSTANCES_PATH = ODK_ROOT + File.separator + "instances";
-    public static final String CACHE_PATH = ODK_ROOT + File.separator + ".cache";
-    public static final String METADATA_PATH = ODK_ROOT + File.separator + "metadata";
-    public static final String TMPFILE_PATH = CACHE_PATH + File.separator + "tmp.jpg";
-    public static final String TMPDRAWFILE_PATH = CACHE_PATH + File.separator + "tmpDraw.jpg";
-    public static final String TMPXML_PATH = CACHE_PATH + File.separator + "tmp.xml";
-    public static final String LOG_PATH = ODK_ROOT + File.separator + "log";
+  static {
+    PRNGFixes.apply();
+  }
 
-    public static final String DEFAULT_FONTSIZE = "21";
-    public static final String OFFLINE_LAYERS = ODK_ROOT + File.separator + "layers";
+  // Storage paths
+  public static final String ODK_ROOT = Environment.getExternalStorageDirectory()
+      + File.separator + "odk";
+  public static final String FORMS_PATH = ODK_ROOT + File.separator + "forms";
+  public static final String INSTANCES_PATH = ODK_ROOT + File.separator + "instances";
+  public static final String CACHE_PATH = ODK_ROOT + File.separator + ".cache";
+  public static final String METADATA_PATH = ODK_ROOT + File.separator + "metadata";
+  public static final String TMPFILE_PATH = CACHE_PATH + File.separator + "tmp.jpg";
+  public static final String TMPDRAWFILE_PATH = CACHE_PATH + File.separator + "tmpDraw.jpg";
+  public static final String TMPXML_PATH = CACHE_PATH + File.separator + "tmp.xml";
+  public static final String LOG_PATH = ODK_ROOT + File.separator + "log";
 
-    // share all session cookies across all sessions...
-    private CookieStore cookieStore = new BasicCookieStore();
-    // retain credentials for 7 minutes...
-    private CredentialsProvider credsProvider = new AgingCredentialsProvider(7 * 60 * 1000);
-    private ActivityLogger mActivityLogger;
-    private FormController mFormController = null;
-    private ExternalDataManager externalDataManager;
+  public static final String DEFAULT_FONTSIZE = "21";
+  public static final String OFFLINE_LAYERS = ODK_ROOT + File.separator + "layers";
 
-    private static Collect singleton = null;
+  // share all session cookies across all sessions...
+  private CookieStore cookieStore = new BasicCookieStore();
+  // retain credentials for 7 minutes...
+  private CredentialsProvider credsProvider = new AgingCredentialsProvider(7 * 60 * 1000);
+  private ActivityLogger mActivityLogger;
+  private FormController mFormController = null;
+  private ExternalDataManager externalDataManager;
 
-    public static Collect getInstance() {
-        return singleton;
+  // Note that if you are building to deploy this, either remove the tracking
+  // logic or include google-services.json in the /app/ directory.
+  private Tracker mTracker;
+
+  private static Collect singleton = null;
+
+  public static Collect getInstance() {
+    return singleton;
+  }
+
+  public ActivityLogger getActivityLogger() {
+    return mActivityLogger;
+  }
+
+  public FormController getFormController() {
+    return mFormController;
+  }
+
+  public void setFormController(FormController controller) {
+    mFormController = controller;
+  }
+
+  public ExternalDataManager getExternalDataManager() {
+    return externalDataManager;
+  }
+
+  public void setExternalDataManager(ExternalDataManager externalDataManager) {
+    this.externalDataManager = externalDataManager;
+  }
+
+  public static int getQuestionFontsize() {
+    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(Collect
+        .getInstance());
+    String question_font = settings.getString(PreferencesActivity.KEY_FONT_SIZE,
+        Collect.DEFAULT_FONTSIZE);
+    int questionFontsize = Integer.valueOf(question_font);
+    return questionFontsize;
+  }
+
+  public String getVersionedAppName() {
+    String versionDetail = "";
+    try {
+      PackageInfo pinfo;
+      pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+      int versionNumber = pinfo.versionCode;
+      String versionName = pinfo.versionName;
+      versionDetail = " " + versionName + " (" + versionNumber + ")";
+    } catch (NameNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return getString(R.string.app_name) + versionDetail;
+  }
+
+  /**
+   * Creates required directories on the SDCard (or other external storage)
+   *
+   * @throws RuntimeException if there is no SDCard or the directory exists as a non directory
+   */
+  public static void createODKDirs() throws RuntimeException {
+    String cardstatus = Environment.getExternalStorageState();
+    if (!cardstatus.equals(Environment.MEDIA_MOUNTED)) {
+      throw new RuntimeException(Collect.getInstance().getString(R.string.sdcard_unmounted, cardstatus));
     }
 
-    public ActivityLogger getActivityLogger() {
-        return mActivityLogger;
-    }
+    String[] dirs = {
+        ODK_ROOT, FORMS_PATH, INSTANCES_PATH, CACHE_PATH, METADATA_PATH, OFFLINE_LAYERS
+    };
 
-    public FormController getFormController() {
-        return mFormController;
-    }
-
-    public void setFormController(FormController controller) {
-        mFormController = controller;
-    }
-
-    public ExternalDataManager getExternalDataManager() {
-        return externalDataManager;
-    }
-
-    public void setExternalDataManager(ExternalDataManager externalDataManager) {
-        this.externalDataManager = externalDataManager;
-    }
-
-    public static int getQuestionFontsize() {
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(Collect
-                .getInstance());
-        String question_font = settings.getString(PreferencesActivity.KEY_FONT_SIZE,
-                Collect.DEFAULT_FONTSIZE);
-        int questionFontsize = Integer.valueOf(question_font);
-        return questionFontsize;
-    }
-
-    public String getVersionedAppName() {
-        String versionDetail = "";
-        try {
-            PackageInfo pinfo;
-            pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            int versionNumber = pinfo.versionCode;
-            String versionName = pinfo.versionName;
-            versionDetail = " " + versionName + " (" + versionNumber + ")";
-        } catch (NameNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    for (String dirName : dirs) {
+      File dir = new File(dirName);
+      if (!dir.exists()) {
+        if (!dir.mkdirs()) {
+          RuntimeException e =
+              new RuntimeException("ODK reports :: Cannot create directory: "
+                  + dirName);
+          throw e;
         }
-        return getString(R.string.app_name) + versionDetail;
-    }
-
-    /**
-     * Creates required directories on the SDCard (or other external storage)
-     *
-     * @throws RuntimeException if there is no SDCard or the directory exists as a non directory
-     */
-    public static void createODKDirs() throws RuntimeException {
-        String cardstatus = Environment.getExternalStorageState();
-        if (!cardstatus.equals(Environment.MEDIA_MOUNTED)) {
-            throw new RuntimeException(Collect.getInstance().getString(R.string.sdcard_unmounted, cardstatus));
+      } else {
+        if (!dir.isDirectory()) {
+          RuntimeException e =
+              new RuntimeException("ODK reports :: " + dirName
+                  + " exists, but is not a directory");
+          throw e;
         }
-
-        String[] dirs = {
-                ODK_ROOT, FORMS_PATH, INSTANCES_PATH, CACHE_PATH, METADATA_PATH,OFFLINE_LAYERS
-        };
-
-        for (String dirName : dirs) {
-            File dir = new File(dirName);
-            if (!dir.exists()) {
-                if (!dir.mkdirs()) {
-                    RuntimeException e =
-                            new RuntimeException("ODK reports :: Cannot create directory: "
-                                    + dirName);
-                    throw e;
-                }
-            } else {
-                if (!dir.isDirectory()) {
-                    RuntimeException e =
-                            new RuntimeException("ODK reports :: " + dirName
-                                    + " exists, but is not a directory");
-                    throw e;
-                }
-            }
-        }
+      }
     }
+  }
 
+  /**
+   * Predicate that tests whether a directory path might refer to an
+   * ODK Tables instance data directory (e.g., for media attachments).
+   *
+   * @param directory
+   * @return
+   */
+  public static boolean isODKTablesInstanceDataDirectory(File directory) {
     /**
-     * Predicate that tests whether a directory path might refer to an
-     * ODK Tables instance data directory (e.g., for media attachments).
-     *
-     * @param directory
-     * @return
+     * Special check to prevent deletion of files that
+     * could be in use by ODK Tables.
      */
-    public static boolean isODKTablesInstanceDataDirectory(File directory) {
-		/**
-		 * Special check to prevent deletion of files that
-		 * could be in use by ODK Tables.
-		 */
-    	String dirPath = directory.getAbsolutePath();
-    	if ( dirPath.startsWith(Collect.ODK_ROOT) ) {
-    		dirPath = dirPath.substring(Collect.ODK_ROOT.length());
-    		String[] parts = dirPath.split(File.separator);
-    		// [appName, instances, tableId, instanceId ]
-    		if ( parts.length == 4 && parts[1].equals("instances") ) {
-    			return true;
-    		}
-    	}
-    	return false;
-	}
-
-    /**
-     * Construct and return a session context with shared cookieStore and credsProvider so a user
-     * does not have to re-enter login information.
-     *
-     * @return
-     */
-    public synchronized HttpContext getHttpContext() {
-
-        // context holds authentication state machine, so it cannot be
-        // shared across independent activities.
-        HttpContext localContext = new BasicHttpContext();
-
-        localContext.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
-        localContext.setAttribute(HttpClientContext.CREDS_PROVIDER, credsProvider);
-
-        return localContext;
+    String dirPath = directory.getAbsolutePath();
+    if (dirPath.startsWith(Collect.ODK_ROOT)) {
+      dirPath = dirPath.substring(Collect.ODK_ROOT.length());
+      String[] parts = dirPath.split(File.separator);
+      // [appName, instances, tableId, instanceId ]
+      if (parts.length == 4 && parts[1].equals("instances")) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    public CredentialsProvider getCredentialsProvider() {
-        return credsProvider;
+  /**
+   * Construct and return a session context with shared cookieStore and credsProvider so a user
+   * does not have to re-enter login information.
+   *
+   * @return
+   */
+  public synchronized HttpContext getHttpContext() {
+
+    // context holds authentication state machine, so it cannot be
+    // shared across independent activities.
+    HttpContext localContext = new BasicHttpContext();
+
+    localContext.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
+    localContext.setAttribute(HttpClientContext.CREDS_PROVIDER, credsProvider);
+
+    return localContext;
+  }
+
+  public CredentialsProvider getCredentialsProvider() {
+    return credsProvider;
+  }
+
+  public CookieStore getCookieStore() {
+    return cookieStore;
+  }
+
+  @Override
+  public void onCreate() {
+    singleton = this;
+
+    // // set up logging defaults for apache http component stack
+    // Log log;
+    // log = LogFactory.getLog("org.opendatakit.httpclientandroidlib");
+    // log.enableError(true);
+    // log.enableWarn(true);
+    // log.enableInfo(true);
+    // log.enableDebug(true);
+    // log = LogFactory.getLog("org.opendatakit.httpclientandroidlib.wire");
+    // log.enableError(true);
+    // log.enableWarn(false);
+    // log.enableInfo(false);
+    // log.enableDebug(false);
+
+    PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+    super.onCreate();
+
+    PropertyManager mgr = new PropertyManager(this);
+
+    FormController.initializeJavaRosa(mgr);
+
+    mActivityLogger = new ActivityLogger(
+        mgr.getSingularProperty(PropertyManager.DEVICE_ID_PROPERTY));
+  }
+
+  synchronized public Tracker getDefaultTracker() {
+    if (mTracker == null) {
+      GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+      // Don't track if we're using debug builds.
+      analytics.setDryRun(BuildConfig.DEBUG);
+      mTracker = analytics.newTracker(R.xml.global_tracker);
     }
-
-    public CookieStore getCookieStore() {
-        return cookieStore;
-    }
-    
-    @Override
-    public void onCreate() {
-        singleton = this;
-
-        // // set up logging defaults for apache http component stack
-        // Log log;
-        // log = LogFactory.getLog("org.opendatakit.httpclientandroidlib");
-        // log.enableError(true);
-        // log.enableWarn(true);
-        // log.enableInfo(true);
-        // log.enableDebug(true);
-        // log = LogFactory.getLog("org.opendatakit.httpclientandroidlib.wire");
-        // log.enableError(true);
-        // log.enableWarn(false);
-        // log.enableInfo(false);
-        // log.enableDebug(false);
-
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-        super.onCreate();
-
-        PropertyManager mgr = new PropertyManager(this);
-
-        FormController.initializeJavaRosa(mgr);
-        
-        mActivityLogger = new ActivityLogger(
-                mgr.getSingularProperty(PropertyManager.DEVICE_ID_PROPERTY));
-    }
-
+    return mTracker;
+  }
 }
